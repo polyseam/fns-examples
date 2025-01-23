11cbe273c5051bfc5b34d87ca20fac77e95db4c9
Matt Johnston <m@ttjohnston.com>
2025-01-22 22:41:27 -0400

    fix: cowsay by using browser mode

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ built using Deno by leveraging the
 [Supabase Edge Runtime](https://github.com/supabase/edge-runtime).
 
 To get started deploying your own self-hosted functions runtime, checkout
-[CNDI]([https://cndi.run/gh?utm_id=](https://docs.google.com/document/d/1iN9lU8Ikn9HWQOuAgDxOKelTfjmz3V_ZYEJrZs7PrO0/edit)) and our
+[CNDI]([https://cndi.run/gh?utm_id=](https://docs.google.com/document/d/1iN9lU8Ikn9HWQOuAgDxOKelTfjmz3V_ZYEJrZs7PrO0/edit))
+and our
 [fns Template](https://cndi.run/gh?utm_content=gh_readme_link_polyseam_fns&utm_campaign=fns_readme&utm_source=github.com/polyseam/fns&utm_medium=repo&utm_id=5102)
 
 You can also checkout the
--- functions/cowsay/index.ts
+++ functions/cowsay/index.ts
@@ -1,6 +1,6 @@
-import { say } from "npm:cowsay";
+import { say } from "npm:cowsay/build/cowsay.es.js";
 
-Deno.serve((req) => {
+Deno.serve((req: Request): Response => {
   const { pathname } = new URL(req.url);
   if (pathname !== "/cowsay") {
     return new Response("Not Found", {
@@ -8,14 +8,14 @@ Deno.serve((req) => {
     });
   }
   const query = new URL(req.url).searchParams;
-  const message = query.get("message");
+  const text = query.get("message");
 
-  if (!message || typeof message !== "string" || message.length === 0) {
+  if (!text || typeof text !== "string" || text.length === 0) {
     return new Response("Please provide a message", {
       status: 400,
     });
   } else {
-    return new Response(say({ text: message }), {
+    return new Response(say({ text }), {
       headers: {
         "Content-Type": "text/plain",
       },
